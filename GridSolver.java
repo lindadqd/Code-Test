26619e2b3df180a9968a0304d253abeee83e569c
linda <lddo@uio.no>
2025-06-29 12:48:02 +0200

    Horizontal product calculation

--- GridSolver.java
+++ GridSolver.java
@@ -9,6 +9,22 @@ public class GridSolver {
             System.out.println("Grid dimensions: " + grid.length + "x" + grid[0].length);
             System.out.println("First element: " + grid[0][0]);
             System.out.println("Last element: " + grid[19][19]);
+
+            int[][] testgrid = {
+                {1, 2, 3, 4}, //24
+                {5, 6, 7, 8}, //1680
+                {9, 1, 2, 3}, //54
+                {4, 5, 6, 7} //840
+            };
+
+             //Test horisontal produkter med test grid
+            long maxHorizontal = findMaxHorizontal(testgrid);
+            System.out.println("Largest horizontal product: " + maxHorizontal);
+
+            //Test horisontal produkter
+            long maxHorizontal2 = findMaxHorizontal(grid);
+            System.out.println("Largest horizontal product: " + maxHorizontal2);
+
         } catch (IOException e){
             System.out.println("Error: " + e.getMessage());
         }
@@ -26,5 +42,21 @@ public class GridSolver {
         }
         scanner.close();
         return grid;
+    }
+
+    private static long findMaxHorizontal(int[][] grid){
+        long maxProduct = 0;
+
+        for (int row = 0; row < grid.length; row++) {
+            for (int column = 0; column + 3 < grid[row].length; column++) { // siste posisjon må være innenfor gridet
+                long product = (long) grid[row][column] * grid[row][column+1] * grid[row][column+2] * grid[row][column+3];
+                if (product > maxProduct) {
+                    maxProduct = product;
+                }
+            }
+        }
+
+        return maxProduct;
+
     }
 }
